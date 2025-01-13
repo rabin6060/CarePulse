043b14dfe6467c0f4882d1162508a6e9458c8c41
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-13 16:46:31 +0545

    basic correction

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate"
-import fontFamily from "tailwindcss/defaultTheme"
+import {fontFamily} from "tailwindcss/defaultTheme"
 
 
 const config = {
